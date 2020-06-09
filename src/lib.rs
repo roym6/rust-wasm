2f0ec7eeb745908710980dfa1aa9ef05db3e50fb
Rodrigo Manzano <rmanzano@techempower.com>
2020-06-09 15:01:09 -0700

    add pause/play functionality, fps counter, and cell toggling

--- src/lib.rs
+++ src/lib.rs
@@ -17,6 +17,15 @@ pub enum Cell {
     Alive = 1,
 }
 
+impl Cell {
+    fn toggle(&mut self) {
+        *self = match *self {
+            Cell::Dead => Cell::Alive,
+            Cell::Alive => Cell::Dead,
+        };
+    }
+}
+
 #[wasm_bindgen]
 pub struct Universe {
     width: u32,
@@ -51,6 +60,17 @@ impl Universe {
 // Public methods, exported to Javascript
 #[wasm_bindgen]
 impl Universe {
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+
+    pub fn height(&self) -> u32 {
+        self.height
+    }
+
+    pub fn cells(&self) -> *const Cell {
+        self.cells.as_ptr()
+    }
     pub fn tick(&mut self) {
         let mut next = self.cells.clone();
 
@@ -76,8 +96,8 @@ impl Universe {
     }
 
     pub fn new() -> Universe {
-        let width = 64;
-        let height = 64;
+        let width = 128;
+        let height = 128;
 
         let cells = (0..width * height)
             .map(|i| {
@@ -98,6 +118,11 @@ impl Universe {
 
     pub fn render(&self) -> String {
         self.to_string()
+    }
+
+    pub fn toggle_cell(&mut self, row: u32, column: u32) {
+        let idx = self.get_index(row, column);
+        self.cells[idx].toggle();
     }
 }
 
