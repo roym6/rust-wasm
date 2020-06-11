8b651fddbccedee875d11134f60e978037f942dc
Rodrigo Manzano <rmanzano@techempower.com>
2020-06-11 12:09:43 -0700

    implement FPS component in Rust

---
+++ src/fps.rs
@@ -0,0 +1,60 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub struct Fps {
+    element: web_sys::Element,
+    frames: Vec<f64>,
+    last_time_stamp: f64,
+}
+
+#[wasm_bindgen]
+impl Fps {
+    pub fn render(&mut self) {
+        let now = web_sys::window().unwrap().performance().unwrap().now();
+        let delta = now - self.last_time_stamp;
+        self.last_time_stamp = now;
+        let fps = (1f64 / delta) * 1000f64;
+
+        self.frames.push(fps);
+        if self.frames.len() > 100 {
+            self.frames.remove(0);
+        }
+
+        let mut min = std::f64::MAX;
+        let mut max = std::f64::MIN;
+        let mut sum = 0f64;
+        for frame in self.frames.iter() {
+            sum += frame;
+            min = min.min(*frame);
+            max = max.max(*frame);
+        }
+        let mean = sum / (self.frames.len() as f64);
+        self.element.set_text_content(Some(
+            format!(
+                "
+            Frames per Second:
+                     latest = {}
+            avg of last 100 = {}
+            min of last 100 = {}
+            max of last 100 = {}
+        ",
+                fps.round(),
+                mean.round(),
+                min.round(),
+                max.round(),
+            )
+            .as_str(),
+        ));
+    }
+    pub fn new() -> Option<Fps> {
+        let window = web_sys::window()?;
+        let element = window.document()?.get_element_by_id("fps")?;
+        let performance = window.performance()?;
+
+        Some(Fps {
+            element,
+            frames: vec![],
+            last_time_stamp: performance.now(),
+        })
+    }
+}
--- www/index.js
+++ www/index.js
@@ -1,4 +1,4 @@
-import { Universe, Cell } from "wasm-game-of-life";
+import { Universe, Cell, Fps } from "wasm-game-of-life";
 import { memory } from "wasm-game-of-life/wasm_game_of_life_bg";
 
 const CELL_SIZE = 5; // px
@@ -11,42 +11,7 @@ const universe = Universe.new();
 const width = universe.width();
 const height = universe.height();
 
-const fps = new (class {
-  constructor() {
-    this.fps = document.getElementById("fps");
-    this.frames = [];
-    this.lastFrameTimeStamp = performance.now();
-  }
-
-  render() {
-    const now = performance.now();
-    const delta = now - this.lastFrameTimeStamp;
-    this.lastFrameTimeStamp = now;
-    const fps = (1 / delta) * 1000;
-
-    this.frames.push(fps);
-    if (this.frames.length > 100) {
-      this.frames.shift();
-    }
-
-    let min = Infinity;
-    let max = -Infinity;
-    let sum = 0;
-    for (let i = 0; i < this.frames.length; i++) {
-      sum += this.frames[i];
-      min = Math.min(this.frames[i], min);
-      max = Math.max(this.frames[i], max);
-    }
-    let mean = sum / this.frames.length;
-    this.fps.textContent = `
-    Frames per Second:
-             latest = ${Math.round(fps)}
-    avg of last 100 = ${Math.round(mean)}
-    min of last 100 = ${Math.round(min)}
-    max of last 100 = ${Math.round(max)}
-    `.trim();
-  }
-})();
+const fps = Fps.new();
 
 // Give the canvas room for all of our cells and a 1px border
 // around each of them.
